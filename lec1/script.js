5722a36817d831cfb489cd3c9b2d18428a55fe06
gitparii <guptapari40023@gmail.com>
2025-07-01 11:54:53 +0530

    lec1

---
+++ lec1/script.js
@@ -0,0 +1,22 @@
+console.log("helllow world");
+var a =200;
+
+console.log(a);
+
+function abc()
+{
+    console.log("hellow class");
+}
+
+
+
+var b = 3000;
+console.log(b);
+
+setTimeout(()=>
+{
+    console.log("i am settimeout");
+
+},2000);
+
+console.log("end");
